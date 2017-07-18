79b0a7ef54ff9ed7c1100b4808f39657ce74eba8
rangelZZZ <18792561236@163.com>
2017-07-18 16:23:05 +0800

    refactor section-3/practice-3.js

--- workspace/gpst-js-basic-collection-practice/main/section-3/practice-3.js
+++ workspace/gpst-js-basic-collection-practice/main/section-3/practice-3.js
@@ -1,5 +1,57 @@
 'use strict';
 
 module.exports = function createUpdatedCollection(collectionA, objectB) {
-  return '实现练习要求，并改写该行代码。';
+    const resultArray = [];
+
+    for (let i = 0; i < collectionA.length; i++) {
+        if (isNotExists(collectionA[i], resultArray)) {
+            const count = calculateCount(collectionA[i], collectionA);
+            resultArray.push({key: collectionA[i], count: count});
+        }
+    }
+
+    return printResult(resultArray, objectB);
+
+};
+
+function isNotExists(element, resultArray) {
+    for (let i = 0; i < resultArray.length; i++) {
+        if (resultArray[i].key === element) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+function calculateCount(element, collectionA) {
+    let count = 0;
+
+    for (let i = 0; i < collectionA.length; i++) {
+        if (collectionA[i] === element) {
+            count++;
+        }
+    }
+
+    return count;
+}
+
+function printResult(resultArray,object) {
+    for (let i = 0; i < resultArray.length; i++) {
+        if (isExists(resultArray[i].key, object.value)) {
+            resultArray[i].count  -= parseInt(resultArray[i].count / 3 );
+        }
+    }
+
+    return resultArray;
+
+}
+
+function isExists(element, collection) {
+    for (let i = 0; i < collection.length; i++) {
+        if (collection[i] === element) {
+            return true;
+        }
+    }
+    return false;
 }
