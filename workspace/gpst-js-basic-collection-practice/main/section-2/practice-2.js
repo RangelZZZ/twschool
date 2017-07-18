395d37d293d1d79c2d2803a3ab999a1c40f5de82
rangelZZZ <18792561236@163.com>
2017-07-18 14:11:57 +0800

    finished section-2/practice-2

--- workspace/gpst-js-basic-collection-practice/main/section-2/practice-2.js
+++ workspace/gpst-js-basic-collection-practice/main/section-2/practice-2.js
@@ -1,5 +1,42 @@
 'use strict';
 
 module.exports = function countSameElements(collection) {
-  return '实现练习要求，并改写该行代码。';
+    const resultArray = [];
+
+    for (let i = 0; i < collection.length; i++) {
+        if (isNotExist(collection[i], resultArray)) {
+            const result = calculateCount(collection[i], collection);
+            resultArray.push({key: result.key, count: result.count});
+        }
+    }
+
+    return resultArray;
+};
+
+function isNotExist(element, resultArray) {
+    for (let i = 0; i < resultArray.length; i++) {
+        if (resultArray[i].key === element) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+function calculateCount(element, collection) {
+    let count = 0;
+    let key;
+
+    for (let i = 0; i < collection.length; i++) {
+        if (collection[i] === element && element.length === 1) {
+            count++;
+            key = element;
+        }
+        if (element.length > 1) {
+            count = parseInt(element.split("-")[1]);
+            key = element.split("-")[0];
+        }
+    }
+
+    return {key: key, count: count};
 }
