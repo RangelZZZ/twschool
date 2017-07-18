d98cfa90472df787ae0e09862b3615337cbe1c9a
rangelZZZ <18792561236@163.com>
2017-07-18 16:15:49 +0800

    finished section-3/practice-3.js

--- workspace/gpst-js-basic-collection-practice/main/section-2/practice-3.js
+++ workspace/gpst-js-basic-collection-practice/main/section-2/practice-3.js
@@ -1,5 +1,63 @@
 'use strict';
 
 module.exports = function countSameElements(collection) {
-  return '实现练习要求，并改写该行代码。';
+    let middleConnection = [];
+
+    for (let i = 0; i < collection.length; i++) {
+        if (collection[i].length > 1) {
+            const count = collection[i].replace(/[^0-9]/ig, "");
+            const element = collection[i].split('')[0];
+            collection.splice(i, 1);
+            middleConnection = generateNewCollection(element, count, middleConnection);
+        }
+    }
+
+    return generateResultCollection(collection, middleConnection);
+
+};
+
+function generateNewCollection(element, count, middleConnection) {
+    for (let i = 0; i < count; i++) {
+        middleConnection.push(element);
+    }
+
+    return middleConnection;
+}
+
+function generateResultCollection(collection, middleConnection) {
+    const resultArray = [];
+
+    const resultCollection = collection.concat(middleConnection);
+
+    for (let i = 0; i < resultCollection.length; i++) {
+        if (isNotExist(resultCollection[i], resultArray)) {
+            const summary = calculateCount(resultCollection[i], resultCollection);
+            resultArray.push({name: resultCollection[i], summary: summary});
+        }
+    }
+
+    return resultArray;
+}
+
+function isNotExist(element, resultArray) {
+
+    for (let i = 0; i < resultArray.length; i++) {
+        if (element === resultArray[i].name) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+function calculateCount(element, resultCollection) {
+    let count = 0;
+
+    for (let i = 0; i < resultCollection.length; i++) {
+        if (element === resultCollection[i]) {
+            count++;
+        }
+    }
+
+    return count;
 }
