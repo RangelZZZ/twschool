36ab9c5b9f53d3069724ca2ea1bba816a8b8bf5d
rangelZZZ <18792561236@163.com>
2017-07-18 12:08:00 +0800

    finished section-1/practice-2.js

--- workspace/gpst-js-basic-collection-practice/main/section-1/practice-2.js
+++ workspace/gpst-js-basic-collection-practice/main/section-1/practice-2.js
@@ -1,5 +1,33 @@
 'use strict';
 
 module.exports = function collectSameElements(collectionA, collectionB) {
-  return '实现练习要求，并改写该行代码。';
+    const resultArray = [];
+
+    for (let i = 0; i < collectionA.length; i++) {
+        if (isExist(collectionA[i], collectionB)) {
+            resultArray.push(collectionA[i]);
+        }
+    }
+
+    return resultArray;
+};
+
+function isExist(element, collectionB) {
+    for (let i = 0; i < collectionB.length; i++) {
+        if (isExistElement(element, collectionB[i])) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+function isExistElement(element, collection) {
+    for (let i = 0; i < collection.length; i++) {
+        if (collection[i] === element) {
+            return true;
+        }
+    }
+
+    return false;
 }
