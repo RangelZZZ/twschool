29aa7da183a345a2909af2aea8e7f52852aca513
rangelZZZ <18792561236@163.com>
2017-07-21 16:34:53 +0800

    finished practice-4 test one

--- workspace/pos-dojo/oop-dojo/spec/practice_4/main_spec.js
+++ workspace/pos-dojo/oop-dojo/spec/practice_4/main_spec.js
@@ -1,7 +1,12 @@
 "use strict";
+import Person from "../../main/practice_4";
 
-describe("OO入门", function(){
-    it("覆盖父类的方法-2", function(){
+describe("OO入门", function () {
+    it("覆盖父类的方法-1", function () {
+        const person = new Person("guoru", "21");
 
+        const expectString = `My name is guoru.I am 21 years old`;
+        expect(person.introduce()).toBe(expectString);
     });
+
 });
