7717ecca5496ee8503a36b87236ffbdcc3c960cf
rangelZZZ <18792561236@163.com>
2017-07-21 15:32:37 +0800

    finished practice-1 test caase expected time 5 minites actual time 7 minites

--- workspace/pos-dojo/oop-dojo/spec/practice_1/main_spec.js
+++ workspace/pos-dojo/oop-dojo/spec/practice_1/main_spec.js
@@ -1,7 +1,10 @@
 "use strict";
+import Person from "../../main/practice_1";
 
-describe("OO入门", function(){
-    it("写一个类-1", function(){
-
+describe("OO入门", function () {
+    it("写一个类-1", function () {
+        const person = new Person("guoru", "21");
+        const expectString = `My name is guoru.I am 21 years old`;
+        expect(person.introduce()).toBe(expectString);
     });
 });
