6d1c2493e26d24c8b3ae21dee079fb7e47f38c75
rangelZZZ <18792561236@163.com>
2017-07-21 18:28:52 +0800

    finished practice-7 test case one

--- workspace/pos-dojo/oop-dojo/spec/practice_7/main_spec.js
+++ workspace/pos-dojo/oop-dojo/spec/practice_7/main_spec.js
@@ -1,7 +1,10 @@
 "use strict";
 
-describe("OO入门", function(){
-    it("对象之间的交互-1", function(){
+describe("OO入门", function () {
+    it("对象之间交互-1", function () {
+        const person = new Person("guoru", "21");
+        const expectString = `My name is guoru.I am 21 years old.`;
 
+        expect(person.introduce()).toBe(expectString);
     });
 });
