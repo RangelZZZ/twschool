c66108dc80bee2525c21cb6dbc827ee39220a86c
rangelZZZ <18792561236@163.com>
2017-07-21 17:15:51 +0800

    finished practice-4 test two

--- workspace/pos-dojo/oop-dojo/main/practice_5.js
+++ workspace/pos-dojo/oop-dojo/main/practice_5.js
@@ -1,14 +1,29 @@
 'use strict';
 
-class Person{
-    constructor(name,age){
+class Person {
+    constructor(name, age) {
         this.name = name;
         this.age = age;
     }
 
-    introduce(){
-        return `My name is ${this.name}.I am ${this.age} years old`;
+    introduce() {
+        return this.basic_Introduce();
+    }
+
+    basic_Introduce() {
+        return `My name is ${this.name}.I am ${this.age} years old.`;
     }
 }
 
-module.exports = Person;
+class Student extends Person {
+    constructor(name, age, className) {
+        super(name, age);
+        this.className = className;
+    }
+
+    introduce(className) {
+        return `${this.basic_Introduce()}I am at ${this.className}.`;
+    }
+}
+
+module.exports = {Person, Student};
--- workspace/pos-dojo/oop-dojo/spec/practice_5/main_spec.js
+++ workspace/pos-dojo/oop-dojo/spec/practice_5/main_spec.js
@@ -1,12 +1,19 @@
 "use strict";
 
-import Person from "../../main/practice_5";
+import {Person, Student} from "../../main/practice_5";
 
 describe("OO入门", function () {
     it("覆盖父类的方法-1", function () {
         const person = new Person("guoru", "21");
-        const expectString = `My name is guoru.I am 21 years old`;
+        const expectString = `My name is guoru.I am 21 years old.`;
 
         expect(person.introduce()).toBe(expectString);
+    });
+
+    it("覆盖父类的方法-2", function () {
+        const student = new Student("guoru", "21", "Class 2");
+        const expectString = `My name is guoru.I am 21 years old.I am at Class 2.`;
+
+        expect(student.introduce()).toBe(expectString);
     });
 });
