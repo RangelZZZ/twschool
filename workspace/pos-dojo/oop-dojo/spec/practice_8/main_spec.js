d64a4a1769a271ad0409b2d2620986e8a8d52500
rangelZZZ <18792561236@163.com>
2017-07-22 15:39:14 +0800

    finished practice-8 test case one

--- workspace/pos-dojo/oop-dojo/spec/practice_8/main_spec.js
+++ workspace/pos-dojo/oop-dojo/spec/practice_8/main_spec.js
@@ -1,7 +1,12 @@
 "use strict";
 
-describe("OO入门", function(){
-    it("对象之间的交互-2", function(){
+import Person from "../../main/practice_8";
 
+describe("OO入门", function () {
+    it("对象之间交互-8-1", function () {
+        const person = new Person("guoru", "21");
+        const expectString = `My name is guoru.I am 21 years old.`;
+
+        expect(person.introduce()).toBe(expectString);
     });
 });
