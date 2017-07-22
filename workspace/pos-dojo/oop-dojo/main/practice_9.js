834998525b69040ace9eb493567d9cf7bd8e4b5b
rangelZZZ <18792561236@163.com>
2017-07-22 23:14:16 +0800

    finished practice-9 test three

--- workspace/pos-dojo/oop-dojo/main/practice_9.js
+++ workspace/pos-dojo/oop-dojo/main/practice_9.js
@@ -10,6 +10,41 @@ class Person {
     introduce() {
         return `My name is ${this.name}.I am ${this.age} years old.`;
     }
+
+    judgeTheSame(person) {
+        return (this.id === person.id);
+    }
 }
 
-module.exports = Person;
+class Class {
+    constructor(number) {
+        this.number = number;
+    }
+
+    assignLeader(student) {
+        this.leader = student;
+    }
+
+    isLeader(student) {
+        return (this.leader && (student.judgeTheSame(this.leader)));
+    }
+}
+
+class Student extends Person {
+    constructor(name, age, id, classInfo) {
+        super(name, age, id);
+        this.classInfo = classInfo;
+    }
+
+    introduce() {
+        let resultString = `${super.introduce()} I am a Student.`;
+
+        if (this.classInfo.isLeader(this)) {
+            return `${resultString} I am Leader of Class ${this.classInfo.number}.`;
+        }
+
+        return `${resultString} I am at Class ${this.classInfo.number}.`;
+    }
+}
+
+module.exports = {Person, Student, Class};
--- workspace/pos-dojo/oop-dojo/spec/practice_9/main_spec.js
+++ workspace/pos-dojo/oop-dojo/spec/practice_9/main_spec.js
@@ -1,12 +1,34 @@
 "use strict";
 
-import Person from "../../main/practice_9";
+import {Person, Student, Class} from "../../main/practice_9";
 
 describe("OO入门", function () {
     it("对象之间交互-9-1", function () {
-        const person = new Person("guoru", "21", 1);
-        const expectString = `My name is guoru.I am 21 years old.`;
+        const person = new Person("Tom", "21", 1);
+        const expectString = `My name is Tom.I am 21 years old.`;
 
         expect(person.introduce()).toBe(expectString);
     });
+
+    it("对象之间交互-9-2", function () {
+        const classInfo = new Class(2);
+        const student = new Student("Tom", "21", 1, classInfo);
+        const expectString = `My name is Tom.I am 21 years old. I am a Student. I am at Class 2.`;
+
+        expect(student.introduce()).toBe(expectString);
+    });
+
+    it("对象之间交互-9-3", function () {
+        const classInfo = new Class(2);
+        const student = new Student("Tom", "21", 1, classInfo);
+
+        classInfo.assignLeader(new Student("rangel", "22", 2));
+        const expectString = `My name is Tom.I am 21 years old. I am a Student. I am at Class 2.`;
+        expect(student.introduce()).toBe(expectString);
+
+        classInfo.assignLeader(student);
+        const expectedString = `My name is Tom.I am 21 years old. I am a Student. I am Leader of Class 2.`;
+        expect(student.introduce()).toBe(expectedString);
+    })
+
 });
