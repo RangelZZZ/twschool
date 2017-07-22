b762fb6fe6903961c1a18a90eb608924ab1e6ad2
rangelZZZ <18792561236@163.com>
2017-07-22 16:54:41 +0800

    finished practice-9 test case one

---
+++ workspace/pos-dojo/oop-dojo/main/practice_9.js
@@ -0,0 +1,15 @@
+"use strict";
+
+class Person {
+    constructor(name, age, id) {
+        this.name = name;
+        this.age = age;
+        this.id = id;
+    }
+
+    introduce() {
+        return `My name is ${this.name}.I am ${this.age} years old.`;
+    }
+}
+
+module.exports = Person;
--- workspace/pos-dojo/oop-dojo/spec/practice_9/main_spec.js
+++ workspace/pos-dojo/oop-dojo/spec/practice_9/main_spec.js
@@ -1,7 +1,12 @@
 "use strict";
 
-describe("OO入门", function(){
-    it("对象之间的交互-3", function(){
+import Person from "../../main/practice_9";
 
+describe("OO入门", function () {
+    it("对象之间交互-9-1", function () {
+        const person = new Person("guoru", "21", 1);
+        const expectString = `My name is guoru.I am 21 years old.`;
+
+        expect(person.introduce()).toBe(expectString);
     });
 });
