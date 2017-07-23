08bb7dfda3ce1cb0d04aae55904ae4c2f586b228
rangelZZZ <18792561236@163.com>
2017-07-23 16:54:05 +0800

    finished practice-11 test case two

--- workspace/pos-dojo/oop-dojo/main/practice_11.js
+++ workspace/pos-dojo/oop-dojo/main/practice_11.js
@@ -10,6 +10,63 @@ class Person {
     introduce() {
         return `My name is ${this.name}.I am ${this.age} years old.`;
     }
+
+    judgeTheSame(person) {
+        return (this.id === person.id);
+    }
 }
 
-module.exports = Person;
+class Class {
+    constructor(number) {
+        this.number = number;
+        this.students = [];
+    }
+
+    assignLeader(student) {
+        if (!this.isExist(student)) {
+            return `It is not one of us.`;
+        }
+        this.leader = student;
+    }
+
+    isLeader(student) {
+        return this.leader && student.judgeTheSame(this.leader);
+    }
+
+    appendMember(student) {
+        if (!this.isExist(student)) {
+            this.students.push(student);
+        }
+    }
+
+    isExist(student) {
+        for (let stu of this.students) {
+            if (stu.judgeTheSame(student)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+}
+
+class Student extends Person {
+    constructor(name, age, id, classInfo) {
+        super(name, age, id);
+        this.classInfo = classInfo;
+        this.classInfo.appendMember(this);
+    }
+
+    introduce() {
+        let resultString = `${super.introduce()}I am a Student.`;
+
+        if (this.classInfo.isLeader(this)) {
+            return `${resultString}I am Leader of Class ${this.classInfo.number}.`;
+        } else {
+            return `${resultString}I am at Class ${this.classInfo.number}.`;
+        }
+    }
+}
+
+module.exports = {Person, Class, Student};
--- workspace/pos-dojo/oop-dojo/spec/practice_11/main_spec.js
+++ workspace/pos-dojo/oop-dojo/spec/practice_11/main_spec.js
@@ -1,6 +1,6 @@
 "use strict";
 
-import Person from "../../main/practice_11";
+import {Person,Class,Student} from "../../main/practice_11";
 
 describe("OO入门", function () {
     it("对象之间交互-11-1", function () {
@@ -10,5 +10,29 @@ describe("OO入门", function () {
         expect(person.introduce()).toBe(expectString);
     });
 
+    it("对象之间交互-11-1",function () {
+        const student = new Student("Tom", "21", 1,new Class(2));
+        const expectString = `My name is Tom.I am 21 years old.I am a Student.I am at Class 2.`;
+
+        expect(student.introduce()).toBe(expectString);
+
+    });
+
+    it("对象之间交互-10-3", function () {
+        const classInfo = new Class(2);
+        const student = new Student("Tom", "21", 1, classInfo);
+
+        classInfo.assignLeader(new Student("rangel", "22", 2, classInfo));
+        const expectString = `My name is Tom.I am 21 years old.I am a Student.I am at Class 2.`;
+        expect(student.introduce()).toBe(expectString);
+
+        classInfo.assignLeader(student);
+        let expectedString = `My name is Tom.I am 21 years old.I am a Student.I am Leader of Class 2.`;
+        expect(student.introduce()).toBe(expectedString);
+
+        const student1 = new Student("rangel", "22", 4, new Class(1));
+        expectedString = `It is not one of us.`;
+        expect(classInfo.assignLeader(student1)).toBe(expectedString);
+    });
 });
 
