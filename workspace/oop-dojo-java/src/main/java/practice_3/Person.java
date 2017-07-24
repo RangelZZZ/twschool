0cc3e8df28f3eb1f064f08a72bdc38d395e86dbd
rangelZZZ <18792561236@163.com>
2017-07-24 16:15:48 +0800

    finished practice-3 test case two expected time 10 minites actual time 17 minites

--- workspace/oop-dojo-java/src/main/java/practice_3/Person.java
+++ workspace/oop-dojo-java/src/main/java/practice_3/Person.java
@@ -4,12 +4,21 @@ public class Person {
     private final String name;
     private final int age;
 
+    public int getAge() {
+        return age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+
     public Person(String name, int age) {
         this.name = name;
         this.age = age;
-
-
     }
+
+
 
     public String introduce() {
         return "My name is " + this.name + ". I am " + this.age + " years old.\n";
---
+++ workspace/oop-dojo-java/src/main/java/practice_3/Student.java
@@ -0,0 +1,16 @@
+package practice_3;
+
+
+public class Student extends Person {
+
+    private int klass;
+
+    public Student(String name, int age, int klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public String introduce() {
+        return "My name is " + this.getName() + ". I am " + this.getAge() + " years old.I am at Class " + this.klass + "\n ";
+    }
+}
