8f318a1633edca205a9151103d318d80a9697787
rangelZZZ <18792561236@163.com>
2017-07-24 17:51:01 +0800

    finished practice-7 test case three expected time 5 minites actual time 8:23

---
+++ workspace/oop-dojo-java/src/main/java/practice_7/Teacher.java
@@ -0,0 +1,29 @@
+package practice_7;
+
+
+public class Teacher extends Person {
+
+    private int klass = 0;
+
+    public int getKlass() {
+        return klass;
+    }
+
+    public Teacher(String name, int age, int klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+
+    public Teacher(String name, int age) {
+        super(name, age);
+    }
+
+    public String introduce() {
+        if (this.getKlass() != 0) {
+            return super.introduce() + " I am a Teacher. I teach Class " + this.getKlass() + ".";
+
+        }
+        return super.introduce() + " I am a Teacher. I teach No Class.";
+    }
+}
---
+++ workspace/oop-dojo-java/src/test/java/practice_7/TeacherTest.java
@@ -0,0 +1,24 @@
+package practice_7;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class TeacherTest {
+
+    private Teacher teacher;
+
+    @Test
+    public void getTeacherInfoWhenHaveClass() throws Exception {
+        teacher = new Teacher("Tom", 21, 2);
+        assertThat(teacher.introduce(),is("My name is Tom. I am 21 years old. I am a Teacher. I teach Class 2."));
+    }
+
+
+    @Test
+    public void getTeacherInfoWhenNoClass() throws Exception {
+        teacher = new Teacher("Tom", 21);
+        assertThat(teacher.introduce(),is("My name is Tom. I am 21 years old. I am a Teacher. I teach No Class."));
+    }
+}
