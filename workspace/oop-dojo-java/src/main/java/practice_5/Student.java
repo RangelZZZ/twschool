144e1a6c4b5094b1967acc6a58b31bfa7ba54436
rangelZZZ <18792561236@163.com>
2017-07-24 16:53:59 +0800

    finished practice-5 test two

---
+++ workspace/oop-dojo-java/src/main/java/practice_5/Student.java
@@ -0,0 +1,14 @@
+package practice_5;
+
+public class Student extends Person {
+    private int klass;
+
+    public Student(String name, int age, int klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public String introduce(){
+        return "My name is " + this.getName() + ". I am " + this.getAge() + " years old.I am at Class " + this.klass + "\n ";
+    }
+}
