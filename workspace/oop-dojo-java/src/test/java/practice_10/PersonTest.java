33b3727e956b71e12c69ce93f1cae7af7ed396f4
rangelZZZ <18792561236@163.com>
2017-07-24 22:12:53 +0800

    finished practice_10 test case one expected time 5 minites actual time 4:48

---
+++ workspace/oop-dojo-java/src/test/java/practice_10/PersonTest.java
@@ -0,0 +1,20 @@
+package practice_10;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class PersonTest {
+    private Person person;
+
+    @Test
+    public void should_return_person_information() throws Exception {
+        person = new Person("Tom", 21);
+
+        String resultString = "My name is Tom. I am 21 years old.";
+        assertThat(person.introduce(), is(resultString));
+    }
+}
+
+
