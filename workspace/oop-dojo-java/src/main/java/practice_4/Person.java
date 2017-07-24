cafea56fce0c50b4bebc2578167a66f24e84f634
rangelZZZ <18792561236@163.com>
2017-07-24 16:25:44 +0800

    finished practice-4 test case one

---
+++ workspace/oop-dojo-java/src/main/java/practice_4/Person.java
@@ -0,0 +1,17 @@
+package practice_4;
+
+public class Person {
+
+    private int age;
+    private String name;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+
+    public String introduce() {
+        return "My name is " + this.name + ". I am " + this.age + " years old.\n";
+    }
+}
---
+++ workspace/oop-dojo-java/src/test/java/practice_4/PersonTest.java
@@ -0,0 +1,19 @@
+package practice_4;
+
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class PersonTest {
+    private Person person;
+
+    @Test
+    public void getString() throws Exception {
+        person = new Person("Tom", 21);
+
+        String resultString = "My name is Tom. I am 21 years old.\n";
+        assertThat(person.introduce(), is(resultString));
+    }
+}
