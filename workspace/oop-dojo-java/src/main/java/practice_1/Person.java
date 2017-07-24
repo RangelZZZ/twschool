e299ae10967b37f122aab20acf4e57f5bfb5907c
rangelZZZ <18792561236@163.com>
2017-07-24 14:57:24 +0800

    finished practice-1 test case1 expected time 5 minites actual time 10 minites

---
+++ workspace/oop-dojo-java/src/main/java/practice_1/Person.java
@@ -0,0 +1,15 @@
+package practice_1;
+
+public class Person {
+    private final String name;
+    private final String age;
+
+    public Person(String name, String age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String introduce() {
+        return "My name is " + this.name + ". I am " + this.age + " years old.\n";
+    }
+}
