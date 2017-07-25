f2b8f4cbfe84868b47eb5ab48865fe9b21302aeb
rangelZZZ <18792561236@163.com>
2017-07-25 08:55:18 +0800

    finished practice-10 test case four expect time 10 minites actual time one night the reason is variable not intinal

--- workspace/oop-dojo-java/src/main/java/practice_10/Class.java
+++ workspace/oop-dojo-java/src/main/java/practice_10/Class.java
@@ -1,10 +1,21 @@
 package practice_10;
 
+import java.util.ArrayList;
+
 public class Class {
     private int number;
+
+    public Student getLeader() {
+
+        return leader;
+    }
+
     private Student leader;
+    private ArrayList<Student> students = new ArrayList<>();
+
 
     public int getNumber() {
+
         return number;
     }
 
@@ -13,11 +24,39 @@ public class Class {
     }
 
     public void assignLeader(Student student) {
+        if (isExist(student)) {
+            this.leader = student;
+        }
+    }
 
-        this.leader = student;
+    public String judgeLeader() {
+        if (this.getLeader() == null) {
+            return "It is not one of us.";
+        }
+
+        return null;
+    }
+
+    public Boolean isExist(Student student) {
+        for (Student student1 : this.students) {
+            if (student1.isSamePeople(student)) {
+
+                return true;
+            }
+        }
+
+        return false;
     }
 
     public Boolean isLeader(Student student) {
-        return this.leader.isSamePeople(student);
+        if (isExist(student)) {
+            return this.leader.isSamePeople(student);
+        }
+
+        return false;
+    }
+
+    public void appendMember(Student student) {
+        this.students.add(student);
     }
 }
--- workspace/oop-dojo-java/src/test/java/practice_10/StudentTest.java
+++ workspace/oop-dojo-java/src/test/java/practice_10/StudentTest.java
@@ -14,6 +14,7 @@ public class StudentTest {
         Class klass = new Class(2);
 
         student = new Student("Tom", 21, 1, klass);
+        klass.appendMember(student);
         klass.assignLeader(student);
 
         assertThat(student.introduce(), is("My name is Tom. I am 21 years old. I am a Student. I am Leader of Class 2."));
@@ -24,8 +25,19 @@ public class StudentTest {
         Class klass = new Class(2);
 
         student = new Student("Tom", 21, 1, klass);
+        klass.appendMember(new Student("rangel", 21, 2, klass));
         klass.assignLeader(new Student("rangel", 21, 2, klass));
 
         assertThat(student.introduce(), is("My name is Tom. I am 21 years old. I am a Student. I am at Class 2."));
     }
+
+    @Test
+    public void should_return_warning_information_when_student_not_join_class() throws Exception {
+        Class klass = new Class(2);
+        klass.assignLeader(new Student("rangel", 21, 2, klass));
+
+        assertThat(klass.judgeLeader(), is("It is not one of us."));
+    }
+
+
 }
