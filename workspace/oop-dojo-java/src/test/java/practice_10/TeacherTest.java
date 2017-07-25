3ccebd1008ef7b30581d29ea28f9b90ffd6d6363
rangelZZZ <18792561236@163.com>
2017-07-25 09:10:16 +0800

    finishd practice-10 test case five expect time 5 minites actual time 7 minites

---
+++ workspace/oop-dojo-java/src/test/java/practice_10/TeacherTest.java
@@ -0,0 +1,22 @@
+package practice_10;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class TeacherTest {
+    private Teacher teacher;
+    @Test
+    public void getTeacherInfoWhenHaveClass() throws Exception {
+        teacher = new Teacher("Tom", 21,1, new Class(2));
+        assertThat(teacher.introduce(), is("My name is Tom. I am 21 years old. I am a Teacher. I teach Class 2."));
+    }
+
+    @Test
+    public void getTeacherInfoWhenNoClass() throws Exception {
+        teacher = new Teacher("Tom", 21,1);
+        assertThat(teacher.introduce(), is("My name is Tom. I am 21 years old. I am a Teacher. I teach No Class."));
+    }
+
+}
