d22c3a2165ead56d792a9fba4e60cba442b7756e
rangelZZZ <18792561236@163.com>
2017-07-26 23:11:31 +0800

    finished student-score-ms test case two expect time 20 minites actual time 31.42

--- workspace/student-score-ms/src/main/java/Menu.java
+++ workspace/student-score-ms/src/main/java/Menu.java
@@ -5,6 +5,12 @@ public class Menu {
     }
 
     public static Boolean judgeStudentInputFormat(String studentInput) {
+        String studentInputFormat = "([^，]+)，(\\w+)，数学：(\\d+)，语文：(\\d+)，英语：(\\d+)，编程：(\\d+)";
+        if (studentInput.matches(studentInputFormat)) {
+            return true;
+        }
         return false;
     }
+
+
 }
--- workspace/student-score-ms/src/test/java/MenuTest.java
+++ workspace/student-score-ms/src/test/java/MenuTest.java
@@ -33,11 +33,22 @@ public class MenuTest {
 
     @Test
     public void should_return_false_when_student_input_err_information() throws Exception {
-
         String studentInput = "rangel,001";
 
         Boolean result = Menu.judgeStudentInputFormat(studentInput);
 
         assertThat(result, is(false));
     }
+
+    @Test
+    public void should_return_true_when_student_input_right_information() throws Exception {
+
+        String studentInput = "guoru，001，数学：97，语文：95，英语：76，编程：100";
+
+        Boolean result = Menu.judgeStudentInputFormat(studentInput);
+        System.out.println(result);
+
+        assertThat(result, is(true));
+    }
+
 }
