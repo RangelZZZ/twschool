0fe9608e05eb555763055e27c833844634c9924c
rangelZZZ <18792561236@163.com>
2017-08-04 16:41:17 +0800

    finished put method

--- workspace/spring-boot-base/src/main/java/bean/Employee.java
+++ workspace/spring-boot-base/src/main/java/bean/Employee.java
@@ -22,4 +22,12 @@ public class Employee {
     public String getName() {
         return name;
     }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
--- workspace/spring-boot-base/src/main/java/com/student/conroller/EmployeeController.java
+++ workspace/spring-boot-base/src/main/java/com/student/conroller/EmployeeController.java
@@ -3,10 +3,7 @@ package com.student.conroller;
 import bean.Employee;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -17,10 +14,40 @@ public class EmployeeController {
     Map<Integer, Employee> employees = new HashMap<>();
 
     @RequestMapping(value = "/employees", method = RequestMethod.POST)
-    public ResponseEntity<Employee> addEmployee(@RequestBody Employee employee) {
+    public ResponseEntity<Employee> addEmployees(@RequestBody Employee employee) {
         employee.setId(1);
         employees.put(1, employee);
 
         return new ResponseEntity<Employee>(employee, HttpStatus.CREATED);
     }
+
+
+    @RequestMapping(value = "/employees", method = RequestMethod.GET)
+    public ResponseEntity<Map<Integer, Employee>> getEmployees() {
+
+        return new ResponseEntity<Map<Integer, Employee>>(employees, HttpStatus.CREATED);
+    }
+
+    @RequestMapping(value = "/employees/{id}", method = RequestMethod.GET)
+    public ResponseEntity<Map<Integer, Employee>> getEmployeesById(@PathVariable("id") int id) {
+        System.out.println(id);
+        if (employees.get(id) == null) {
+            return new ResponseEntity<>((Map<Integer, Employee>) null, HttpStatus.NOT_FOUND);
+        }
+
+        return new ResponseEntity<Map<Integer, Employee>>(employees, HttpStatus.CREATED);
+    }
+
+    @RequestMapping(value = "/employees/{id}", method = RequestMethod.PUT)
+    public ResponseEntity<Map<Integer, Employee>> update(@PathVariable("id") int id, @RequestBody Employee employee) {
+        if (employees.get(id) == null) {
+            return new ResponseEntity<>((Map<Integer, Employee>) null, HttpStatus.NOT_FOUND);
+        }
+
+        employees.get(1).setName(employee.getName());
+        employees.get(1).setAge(employee.getAge());
+
+        return new ResponseEntity<Map<Integer, Employee>>((Map<Integer, Employee>) null, HttpStatus.NO_CONTENT);
+    }
+
 }
