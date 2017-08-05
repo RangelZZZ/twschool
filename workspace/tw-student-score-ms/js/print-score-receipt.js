d150c3488c9bed5fbaccbcf138e590c38877cedc
rangelZZZ <18792561236@163.com>
2017-08-05 23:08:37 +0800

    finished get student-score-list

--- workspace/tw-student-score-ms/js/print-score-receipt.js
+++ workspace/tw-student-score-ms/js/print-score-receipt.js
@@ -1,12 +1,24 @@
 $(function () {
-    console.log("——————————————————");
-    const studentScoreList = [{
-        "name": "guoru", "studentId": 1, "mathScore": 90,
-        "chineseScore": 90, "programScore": 90, "totalScore": 360, "averageScore": 90
-    }, {
-        "name": "fuhong", "studentId": 1, "mathScore": 90,
-        "chineseScore": 90, "programScore": 90, "totalScore": 360, "averageScore": 90
-    }];
+    const studentScoreList = {
+        studentScoreList: [{
+            "username": "guoru", "studentId": 1, "mathScore": 90,
+            "chineseScore": 90, "englishScore": 90, "programScore": 90, "totalScore": 360, "averageScore": 90
+        }, {
+            "username": "fuhong", "studentId": 1, "mathScore": 90,
+            "chineseScore": 90, "englishScore": 90, "programScore": 90, "totalScore": 360, "averageScore": 90
+        }],
+        totalScore: 360,
+        medianScore: 90
+    };
+    localStorage.setItem("scoreList", JSON.stringify(studentScoreList));
 
-    localStorage.setItem("scoreList", studentScoreList);
+    const selectedStudentScoreList = JSON.parse(localStorage.getItem("scoreList"));
+    console.log(selectedStudentScoreList);
+
+    displaySelectScoreStudent(selectedStudentScoreList.studentScoreList);
 });
+
+function displaySelectScoreStudent(studentScoreList) {
+
+    $("#myTemplate").tmpl(studentScoreList).appendTo("#rows");
+}
