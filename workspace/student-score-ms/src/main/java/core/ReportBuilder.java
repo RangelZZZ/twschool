ca17b5395fc0a6b46e5d0aacc87c6f3a236b9b8f
rangelZZZ <18792561236@163.com>
2017-08-08 18:38:18 +0800

    refacor student-score-ms Reportbuilder getTotalScore and add test

--- workspace/student-score-ms/src/main/java/core/ReportBuilder.java
+++ workspace/student-score-ms/src/main/java/core/ReportBuilder.java
@@ -2,13 +2,36 @@ package core;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class ReportBuilder {
     private List<ReportItem> reportItemList = new ArrayList<ReportItem>();
+    private int totalScore = 0;
 
     public ReportBuilder(List<ReportItem> reportItemList) {
         this.reportItemList = reportItemList;
     }
 
+    public int getReportTotalScore() {
+        for (ReportItem reportItem : reportItemList) {
+            totalScore += reportItem.getTotalScore();
+        }
 
+        return totalScore;
+    }
+
+    public int getMedianScore() {
+        List<ReportItem> sortedItems = reportItemList.stream()
+                .sorted((item1, item2) -> (int) (item1.getTotalScore() - item2.getTotalScore()))
+                .collect(Collectors.toList());
+
+        int medianSize = sortedItems.size() % 2;
+        if (medianSize == 1) {
+            return sortedItems.get(sortedItems.size() / 2).getTotalScore();
+        } else {
+            return (sortedItems.get(sortedItems.size() / 2 - 1).getTotalScore() +
+                    sortedItems.get(sortedItems.size() / 2).getTotalScore()) / 2;
+        }
+
+    }
 }
---
+++ workspace/student-score-ms/src/test/java/core/ReportBuilderTest.java
@@ -0,0 +1,27 @@
+package core;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class ReportBuilderTest {
+
+    @Test
+    public void should_calculate_all_student_total_score() throws Exception {
+
+        List<ReportItem> reportItemList = new ArrayList<ReportItem>();
+        ReportItem reportItem1 = new ReportItem(new Student("guoru", "001", 100, 100, 100, 100));
+        ReportItem reportItem2 = new ReportItem(new Student("guoru", "002", 100, 100, 100, 100));
+        reportItemList.add(reportItem1);
+        reportItemList.add(reportItem2);
+
+        ReportBuilder reportBuilder = new ReportBuilder(reportItemList);
+
+        assertThat(reportBuilder.getReportTotalScore(), is(800));
+
+    }
+}
