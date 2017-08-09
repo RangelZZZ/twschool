9b6f7b7658be3debd2556ef665666a686e9316e1
rangelZZZ <18792561236@163.com>
2017-08-09 10:32:24 +0800

    add validate to student score form

---
+++ workspace/tw-student-score-ms/web/add-student-score.js
@@ -0,0 +1,62 @@
+function addStudent(studentInformation) {
+    console.log(studentInformation);
+}
+
+$(document).ready(function () {
+    $("#student-score-form").validate({
+        rules: {
+            username: "required",
+            mathScore: {
+                required: true,
+                min: 0,
+                max: 100
+            },
+            chineseScore: {
+                required: true,
+                min: 0,
+                max: 100
+            },
+            englishScore: {
+                required: true,
+                min: 0,
+                max: 100
+            },
+            programScore: {
+                required: true,
+                min: 0,
+                max: 100
+            },
+        },
+        messages: {
+            username: "用户名不能为空",
+            mathScore: {
+                required: "成绩不能为空",
+                min: "成绩最低为0分",
+                max: "成绩最高为100分"
+            },
+            chineseScore: {
+                required: "成绩不能为空",
+                min: "成绩最低为0分",
+                max: "成绩最高为100分"
+            },
+            englishScore: {
+                required: "成绩不能为空",
+                min: "成绩最低为0分",
+                max: "成绩最高为100分"
+            },
+            programScore: {
+                required: "成绩不能为空",
+                min: "成绩最低为0分",
+                max: "成绩最高为100分"
+            },
+        },
+
+        submitHandler: function (form) {
+            event.preventDefault();
+            const studentInformation = $("#student-form").serializeArray();
+            addStudent(studentInformation);
+            form.submit();
+        }
+    });
+
+});
